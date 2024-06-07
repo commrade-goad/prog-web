6e2fc5d6594e6b9a9ca39de92ac9eb1f2736fcd0
Fernando Perry <fernandoperry1234@gmail.com>
2024-06-07 14:20:52 +0700

    fixed user login

--- login.php
+++ login.php
@@ -9,21 +9,23 @@ if (isset($_POST["login"])) {
     $results = $db->query('select name, password from users;');
     while ($row = $results->fetchArray()) {
         array_push($nama, $row["name"]);
+        echo $row["name"];
         array_push($pass, $row["password"]);
     }
-    if ($_POST["username"] == $nama[0] && $_POST["password"] == $pass[0]) {
-        $_SESSION["start"] = true;
-        echo "<script>window.location=\"dashboard.php\"</script>";
-    } else{
-        echo "<script>alert(\"Username atau password salah!\")</script>";
-        echo "<script>window.location=\"index.php\"</script>";
+    for ($i=0; $i < count($nama); $i++) { 
+        if ($_POST["username"] == $nama[$i] && $_POST["password"] == $pass[$i]) {
+            $_SESSION["start"] = true;
+            echo "<script>window.location=\"dashboard.php\"</script>";
+        }
     }
+    echo "<script>alert(\"Username atau password salah!\")</script>";
+    echo "<script>window.location=\"index.php\"</script>";
 }
 elseif (isset($_POST["reg"])) {
     $email = $_POST["email"];
     $username = $_POST["username"];
     $password = $_POST["password"];
-    $new_str = "insert into users (email, name, password) values (\"' . $email . '\",\"' $username . '\",\"' . $password . '\");";
+    $new_str = "insert into users (email, name, password) values (\"" . $email . "\",\"" . $username . "\",\"" . $password . "\");";
 
     $db->query($new_str);
     echo "<script>alert(\"User baru telah teregister.\")</script>";
